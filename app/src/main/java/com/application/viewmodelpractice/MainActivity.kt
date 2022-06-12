0def382d57458bdf5d64179c068b622a29a60cff
KIMMKyungsik <93589990+KIMMKyungsik@users.noreply.github.com>
2022-06-12 19:46:52 +0900

    뷰모델에 어댑터 연결 완료

--- app/src/main/java/com/application/viewmodelpractice/MainActivity.kt
+++ app/src/main/java/com/application/viewmodelpractice/MainActivity.kt
@@ -2,8 +2,13 @@ package com.application.viewmodelpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.application.viewmodelpractice.adapter.CustomAdapter
+import com.application.viewmodelpractice.model.Plant
 import com.application.viewmodelpractice.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +19,16 @@ class MainActivity : AppCompatActivity() {
 
         val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
+        val rv = findViewById<RecyclerView>(R.id.rv)
+
         viewModel.getAllData()
+
+        viewModel.result.observe(this, Observer {
+
+            val custonAdapter = CustomAdapter(this,it)
+            rv.adapter = custonAdapter
+            rv.layoutManager = LinearLayoutManager(this)
+        })
 
 
     }
--- app/src/main/java/com/application/viewmodelpractice/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/application/viewmodelpractice/viewmodel/MainViewModel.kt
@@ -1,9 +1,12 @@
 package com.application.viewmodelpractice.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.application.viewmodelpractice.api.MyApi
 import com.application.viewmodelpractice.api.RetrofitInstance
+import com.application.viewmodelpractice.model.Plant
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.create
@@ -13,9 +16,15 @@ class MainViewModel : ViewModel() {
 
     val retrofitInstance: MyApi = RetrofitInstance.getInstance().create(MyApi::class.java)
 
+    private val _result = MutableLiveData<List<Plant>>()
+    val result : LiveData<List<Plant>>
+            get() = _result
+
 
     fun getAllData() = viewModelScope.launch {
-        retrofitInstance.getAllPlant().toString()
+        _result.value = retrofitInstance.getAllPlant()
+
+
 
     }
 
