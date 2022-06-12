68d42f6da1df03f2bb4f8cf94b836acec3d039c8
KIMMKyungsik <93589990+KIMMKyungsik@users.noreply.github.com>
2022-06-12 19:22:41 +0900

    리사이크러뷰 어댑터

--- app/src/main/java/com/application/viewmodelpractice/MainActivity.kt
+++ app/src/main/java/com/application/viewmodelpractice/MainActivity.kt
@@ -2,10 +2,20 @@ package com.application.viewmodelpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.application.viewmodelpractice.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        viewModel.getAllData()
+
+
     }
 }
--- app/src/main/java/com/application/viewmodelpractice/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/application/viewmodelpractice/viewmodel/MainViewModel.kt
@@ -1,9 +1,23 @@
 package com.application.viewmodelpractice.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.application.viewmodelpractice.api.MyApi
+import com.application.viewmodelpractice.api.RetrofitInstance
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.create
 
 class MainViewModel : ViewModel() {
 
+
+    val retrofitInstance: MyApi = RetrofitInstance.getInstance().create(MyApi::class.java)
+
+
+    fun getAllData() = viewModelScope.launch {
+        retrofitInstance.getAllPlant().toString()
+
+    }
 
 
 }
